690861fc587c80a4ef84d5317afaa39f96736e7a
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-01-01 12:23:57 -0500

    Add Report class

---
+++ semantic/Report.cpp
@@ -0,0 +1,7 @@
+//
+// Created by frnd on 12/30/16.
+//
+
+#include "Report.hpp"
+
+using namespace semantic;
---
+++ semantic/Report.hpp
@@ -0,0 +1,18 @@
+//
+// Created by frnd on 12/30/16.
+//
+
+#ifndef BENGAL_REPORT_HPP
+#define BENGAL_REPORT_HPP
+
+namespace semantic
+{
+
+class Report
+{
+
+};
+
+} // semantic namespace
+
+#endif //BENGAL_REPORT_HPP
