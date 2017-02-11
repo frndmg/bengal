24855d9345e3fd418585d2b705019611bb292840
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-02-10 22:10:40 -0500

    Remove not important comments

--- ast/Expr.hpp
+++ ast/Expr.hpp
@@ -1,10 +1,6 @@
 #ifndef EXPR_HPP
 #define EXPR_HPP
 
-//#include <llvm/IR/Value.h>
-//#include <llvm/IR/IRBuilder.h>
-//#include <llvm/IR/Module.h>
-
 #include "Node.hpp"
 
 namespace sem = semantic;
