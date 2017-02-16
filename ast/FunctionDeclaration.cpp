fda50e91a2c65e41ceb8061a99a3873c5fa1f581
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-02-16 13:51:52 -0500

    Reformat FunctionaDeclaration

--- ast/FunctionDeclaration.cpp
+++ ast/FunctionDeclaration.cpp
@@ -3,17 +3,10 @@
 using namespace ast;
 
 FunctionDeclaration::FunctionDeclaration(
-    const std::shared_ptr<Id>& id,
-    const std::shared_ptr<TypeFields>& arguments,
-    const std::shared_ptr<Expr>& body,
-    const std::shared_ptr<Id>& retType)
-    : m_id(id)
-    , m_arguments(arguments)
-    , m_body(body)
-    , m_retType(retType)
+    const std::shared_ptr<Id>& id, const std::shared_ptr<TypeFields>& arguments,
+    const std::shared_ptr<Expr>& body, const std::shared_ptr<Id>& retType )
+    : m_id( id ), m_arguments( arguments ), m_body( body ), m_retType( retType )
 {
 }
 
-bool ast::FunctionDeclaration::checkSemantic(Scope& scope, Report& report)
-{
-}
+bool ast::FunctionDeclaration::checkSemantic( Scope& scope, Report& report ) {}
--- ast/FunctionDeclaration.hpp
+++ ast/FunctionDeclaration.hpp
@@ -5,25 +5,26 @@
 #include "Node.hpp"
 #include "TypeFields.hpp"
 
-namespace ast {
+namespace ast
+{
 
-class FunctionDeclaration : public Node {
+class FunctionDeclaration : public Node
+{
 public:
-    FunctionDeclaration(const std::shared_ptr<Id>& id,
-        const std::shared_ptr<TypeFields>& arguments,
-        const std::shared_ptr<Expr>& body,
-        const std::shared_ptr<Id>& retType = nullptr);
+    FunctionDeclaration( const std::shared_ptr<Id>&         id,
+                         const std::shared_ptr<TypeFields>& arguments,
+                         const std::shared_ptr<Expr>&       body,
+                         const std::shared_ptr<Id>&         retType = nullptr );
 
 private:
-    std::shared_ptr<Id> m_id;
+    std::shared_ptr<Id>         m_id;
     std::shared_ptr<TypeFields> m_arguments;
-
-    std::shared_ptr<Expr> m_body;
-    std::shared_ptr<Id> m_retType;
+    std::shared_ptr<Expr>       m_body;
+    std::shared_ptr<Id>         m_retType;
 
     // Node interface
 public:
-    virtual bool checkSemantic(Scope& scope, Report& report) override;
+    virtual bool checkSemantic( Scope& scope, Report& report ) override;
 };
 
 } // ast namespace
