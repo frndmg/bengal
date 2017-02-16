90d8a2cae5f20496a1bf32615613fd4dc5b41182
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-02-16 14:18:44 -0500

    Format LexExpr

--- ast/LetExpr.cpp
+++ ast/LetExpr.cpp
@@ -2,16 +2,11 @@
 
 using namespace ast;
 
-LetExpr::LetExpr(const std::shared_ptr<DeclarationList>& scope,
-                 const std::shared_ptr<ExprSeqExpr>& expr_seq,
-                 const Position& pos) :
-    RValue( pos ),
-    m_scope(scope),
-    m_expr_seq(expr_seq)
+LetExpr::LetExpr( const std::shared_ptr<DeclarationList>& scope,
+                  const std::shared_ptr<ExprSeqExpr>&     expr_seq,
+                  const Position&                         pos )
+    : RValue( pos ), m_scope( scope ), m_expr_seq( expr_seq )
 {
 }
 
-bool LetExpr::checkSemantic(Node::Scope &scope, Node::Report &report)
-{
-
-}
+bool LetExpr::checkSemantic( Node::Scope& scope, Node::Report& report ) {}
--- ast/LetExpr.hpp
+++ ast/LetExpr.hpp
@@ -1,9 +1,9 @@
 #ifndef LETEXPR_HPP
 #define LETEXPR_HPP
 
-#include "RValue.hpp"
 #include "DeclarationList.hpp"
 #include "ExprSeqExpr.hpp"
+#include "RValue.hpp"
 
 namespace ast
 {
@@ -11,24 +11,24 @@ namespace ast
 class LetExpr : public RValue
 {
 public:
-    LetExpr(const std::shared_ptr<DeclarationList>& scope,
-            const std::shared_ptr<ExprSeqExpr>& expr_seq,
-            const Position& pos = { 0, 0, 0, 0 });
+    LetExpr( const std::shared_ptr<DeclarationList>& scope,
+             const std::shared_ptr<ExprSeqExpr>&     expr_seq,
+             const Position&                         pos = {0, 0, 0, 0} );
 
 private:
     std::shared_ptr<DeclarationList> m_scope;
-    std::shared_ptr<ExprSeqExpr> m_expr_seq;
+    std::shared_ptr<ExprSeqExpr>     m_expr_seq;
 
     // Node interface
 public:
-    virtual bool checkSemantic(Scope &scope, Report &report) override;
+    virtual bool checkSemantic( Scope& scope, Report& report ) override;
 
     // Node interface
 public:
     virtual operator std::string() const override
     {
-        return "LetExpr( " + static_cast<std::string>( *m_scope )
-                + ", " + static_cast<std::string>( *m_expr_seq ) + " )";
+        return "LetExpr( " + static_cast<std::string>( *m_scope ) + ", "
+               + static_cast<std::string>( *m_expr_seq ) + " )";
     }
 };
 
