b8ef7590f38ca31aecc430267809a7649260e425
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-03-06 20:09:38 -0500

    Reformat code

--- ast/LetExpr.cpp
+++ ast/LetExpr.cpp
@@ -2,11 +2,22 @@
 
 using namespace ast;
 
-LetExpr::LetExpr( const std::shared_ptr<DeclarationList>& scope,
-                  const std::shared_ptr<ExprSeqExpr>&     expr_seq,
-                  const Position&                         pos )
-    : RValue( pos ), m_scope( scope ), m_expr_seq( expr_seq )
+LetExpr::LetExpr(
+        const std::shared_ptr<DeclarationList>& scope,
+        const std::shared_ptr<ExprSeqExpr>& expr_seq,
+        const Position& pos )
+        : RValue( pos )
+        , m_scope( scope )
+        , m_expr_seq( expr_seq )
 {
 }
 
-bool LetExpr::checkSemantic( Node::Scope& scope, Node::Report& report ) {}
+bool LetExpr::checkSemantic( Node::Scope& scope, Node::Report& report )
+{
+}
+
+LetExpr::operator std::string() const
+{
+    return "LetExpr( " + static_cast<std::string>( *m_scope ) + ", "
+           + static_cast<std::string>( *m_expr_seq ) + " )";
+}
