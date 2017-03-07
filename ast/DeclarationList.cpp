fd99a73e5c73e7b269b7e58d402bc62c6b6811ac
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-03-06 19:56:04 -0500

    Reformat code

--- ast/DeclarationList.cpp
+++ ast/DeclarationList.cpp
@@ -2,11 +2,33 @@
 
 using namespace ast;
 
-DeclarationList::DeclarationList( const Position& pos ) : Node( pos ), vector()
+DeclarationList::DeclarationList( const Position& pos )
+        : Node( pos ), vector()
 {
 }
 
-bool ast::DeclarationList::checkSemantic( Node::Scope&  scope,
-                                          Node::Report& report )
+bool DeclarationList::checkSemantic(
+        Node::Scope& scope,
+        Node::Report& report )
 {
+}
+
+DeclarationList::operator std::string() const
+{
+    static std::string s;
+
+    if ( s.empty() )
+    {
+        s += "DeclarationList( { ";
+        auto i = this->begin();
+        if ( i != this->end() )
+        {
+            s += static_cast<std::string>( **( i++ ) );
+            for ( ; i != this->end(); i++ )
+                s += ", " + static_cast<std::string>( **i );
+        }
+        s += " } )";
+    }
+
+    return s;
 }
--- ast/DeclarationList.hpp
+++ ast/DeclarationList.hpp
@@ -9,34 +9,18 @@
 namespace ast
 {
 
-class DeclarationList : public Node,
-                        public std::vector<std::shared_ptr<DeclarationScope> >
+class DeclarationList
+        : public Node,
+          public std::vector<std::shared_ptr<DeclarationScope> >
 {
 public:
-    DeclarationList( const Position& pos = {0, 0, 0, 0} );
+    DeclarationList( const Position& pos = { 0, 0, 0, 0 } );
 
     // Node interface
 public:
     virtual bool checkSemantic( Scope& scope, Report& report ) override;
-    virtual operator std::string() const override
-    {
-        static std::string s;
 
-        if ( s.empty() )
-        {
-            s += "DeclarationList( { ";
-            auto i = this->begin();
-            if ( i != this->end() )
-            {
-                s += static_cast<std::string>( **( i++ ) );
-                for ( ; i != this->end(); i++ )
-                    s += ", " + static_cast<std::string>( **i );
-            }
-            s += " } )";
-        }
-
-        return s;
-    }
+    virtual operator std::string() const override;
 };
 
 } // ast namespace
