bba3ba27e55fba19d2c3c7019e13d29e2fbfdde6
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-03-06 20:26:26 -0500

    Implement LetExpr::checkSemantic

--- ast/LetExpr.cpp
+++ ast/LetExpr.cpp
@@ -14,14 +14,29 @@ LetExpr::LetExpr(
 
 bool LetExpr::checkSemantic( Scope& scope, Report& report )
 {
+    bool ok( true );
 
+    // The execution of the let expression occurs in a new scope
+    Scope new_scope( &scope );
+    ok = this->scope()->checkSemantic( new_scope, report ) and ok;
+    ok = this->expr_seq()->checkSemantic( new_scope, report ) and ok;
+    // at this time new_scope is gone
+
+    // If the type of the let's return isn't in the above scope then error
+    const auto& ret_type = this->expr_seq()->type()->typeName();
+    if ( scope.getTypeDefOf( ret_type ) == nullptr )
+    {
+        ok = false;
+        report.error( *this, "The type `%s` does not exist in the current scope.", ret_type );
+    }
+    return ok;
 }
 
 LetExpr::operator std::string() const
 {
     return "LetExpr( "
-           + static_cast<std::string>( *m_scope ) + ", "
-           + static_cast<std::string>( *m_expr_seq ) + " )";
+           + static_cast<std::string>( *scope() ) + ", "
+           + static_cast<std::string>( *expr_seq() ) + " )";
 }
 
 const std::shared_ptr<DeclarationList>& LetExpr::scope() const
