c268a7916612a98c496381e02c7af352d106e50f
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-03-28 22:42:54 -0400

    Add space to error print

--- semantic/Report.hpp
+++ semantic/Report.hpp
@@ -18,7 +18,7 @@ public:
     template <typename T, typename ...Args>
     void error(const T& node, const char* format, const Args&... args)
     {
-        std::cerr << node.position();
+        std::cerr << node.position() << " ";
         char str[100]; // TODO: How to change this?
         std::sprintf(str, format, args...);
         std::cerr << str << std::endl;
