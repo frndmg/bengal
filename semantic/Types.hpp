ef00589a7f3b49101eea4fc43918df233f5c80eb
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-04-05 14:12:07 -0400

    Fix Types.hpp header

--- semantic/Types.hpp
+++ semantic/Types.hpp
@@ -1,7 +1,3 @@
-//
-// Created by frnd on 1/4/17.
-//
-
 #ifndef BENGAL_TYPES_HPP
 #define BENGAL_TYPES_HPP
 
@@ -9,17 +5,56 @@
 #include <vector>
 #include <memory>
 
-
 namespace semantic
 {
 
 struct IntType;
 struct StringType;
 struct NilType;
+struct NoneType;
+
+}
+
+namespace std
+{
+
+template <>
+shared_ptr<semantic::IntType> make_shared<semantic::IntType>()
+{
+    static std::shared_ptr<semantic::IntType> int_type;
+    return int_type;
+}
+
+template <>
+shared_ptr<semantic::StringType> make_shared<semantic::StringType>()
+{
+    static std::shared_ptr<semantic::StringType> string_type;
+    return string_type;
+}
+
+template <>
+shared_ptr<semantic::NilType> make_shared<semantic::NilType>()
+{
+    static std::shared_ptr<semantic::NilType> nil_type;
+    return nil_type;
+}
+
+template <>
+shared_ptr<semantic::NoneType> make_shared<semantic::NoneType>()
+{
+    static std::shared_ptr<semantic::NoneType> none_type;
+    return none_type;
+}
+
+}
+
+namespace semantic
+{
 
 struct Type
 {
-    virtual ~Type() {}
+    virtual ~Type()
+    { }
 
     const std::string& typeName() const
     {
@@ -27,65 +62,98 @@ struct Type
     }
 
 protected:
-    Type(const std::string& typeName) : m_typeName( typeName ) {}
+    Type( const std::string& typeName )
+            : m_typeName( typeName )
+    { }
 
 private:
     std::string m_typeName;
 };
 
-struct IntType : Type
+struct IntType
+        : Type
 {
-    IntType() : Type( "int" ) {}
+    IntType()
+            : Type( "int" )
+    { }
 };
 
-struct StringType : Type
+struct StringType
+        : Type
 {
-    StringType() : Type( "string" ) {}
+    StringType()
+            : Type( "string" )
+    { }
 };
 
-struct NilType : Type
+struct NilType
+        : Type
 {
-    NilType() : Type( "nil" ) {}
+    NilType()
+            : Type( "nil" )
+    { }
 };
 
-struct NoneType : Type
+struct NoneType
+        : Type
 {
-    NoneType() : Type( "none" ) {}
+    NoneType()
+            : Type( "none" )
+    { }
 };
 
-struct ArrayType : Type
+struct ArrayType
+        : Type
 {
-    ArrayType(const std::string& typeName, const std::shared_ptr<Type>& type) : Type( typeName ), m_type( type ) {}
+    ArrayType( const std::string& typeName, const std::shared_ptr<Type>& type )
+            : Type( typeName ), m_type( type )
+    { }
 
-    const std::shared_ptr<Type>& type() const { return m_type; }
+    const std::shared_ptr<Type>& type() const
+    { return m_type; }
 
-    void setType(const std::shared_ptr<Type>& type) { m_type = type; }
+    void setType( const std::shared_ptr<Type>& type )
+    { m_type = type; }
 
 private:
     std::shared_ptr<Type> m_type;
 };
 
-struct StructType : Type, std::vector<std::pair<std::string, std::shared_ptr<Type> > >
+struct StructType
+        : Type
 {
-    StructType(const std::string& typeName) :
-        Type( typeName ), vector() { }
-    StructType(const std::string& typeName,
-               std::initializer_list<value_type> init) :
-            Type( typeName ), vector( init ) {}
+    typedef std::vector<std::pair<std::string, std::shared_ptr<Type> > > vector;
+    typedef vector::value_type value_type;
+
+    StructType(
+            const std::string& typeName,
+            std::initializer_list<value_type> init = { } )
+            : Type( typeName ), vector( init )
+    { }
+
+    const vector& body()
+    { return m_body; }
+
+private:
+    vector m_body;
 };
 
-struct AliasType : Type
+struct AliasType
+        : Type
 {
-    AliasType(const std::string& typeName, const std::shared_ptr<Type>& typeAlias) :
-            Type( typeName ),
-            m_typeAlias( typeAlias ) {}
+    AliasType(
+            const std::string& typeName,
+            const std::shared_ptr<Type>& typeAlias )
+            :
+            Type( typeName ), m_typeAlias( typeAlias )
+    { }
 
     const std::shared_ptr<Type>& typeAlias() const
     {
         return m_typeAlias;
     }
 
-    void setTypeAlias(const std::shared_ptr<Type>& typeAlias)
+    void setTypeAlias( const std::shared_ptr<Type>& typeAlias )
     {
         m_typeAlias = typeAlias;
     }
@@ -94,36 +162,40 @@ private:
     std::shared_ptr<Type> m_typeAlias;
 };
 
-struct FunctionType :
-        Type,
-        std::vector<
-            std::pair<std::string, std::shared_ptr<Type> >
-        >
+struct FunctionType
+        : Type
 {
-    FunctionType(const std::string& typeName) : Type( typeName ) { }
+    typedef std::vector<std::pair<std::string, std::shared_ptr<Type> > > vector;
+    typedef vector::value_type value_type;
+
+    FunctionType(
+            const std::shared_ptr<Type>& return_type,
+            const std::string& type_name,
+            std::initializer_list<value_type> init = { } )
+            : Type( type_name ), m_params( init )
+    { }
+
+public:
+    const vector& params()
+    { return m_params; }
+
+    const std::shared_ptr<Type> return_type()
+    { return m_return_type; }
+
+private:
+    vector m_params;
+    std::shared_ptr<Type> m_return_type;
 };
 
-struct ProcedureType : FunctionType
+struct ProcedureType
+        : FunctionType
 {
-    ProcedureType(const std::string& typeName) : FunctionType( typeName ) {}
+    ProcedureType(
+            const std::string& typeName,
+            std::initializer_list<value_type> init = { } )
+            : FunctionType( std::make_shared<NoneType>(), typeName, init )
+    { }
 };
-
-//template <typename Type>
-//Type* type(const std::string& typeName);
-//
-//template <>
-//IntType* type<IntType>(const std::string&)
-//{
-//    static IntType* type = new IntType();
-//    return type;
-//}
-//
-//template <>
-//ArrayType* type<ArrayType>(const std::string& typeName)
-//{
-////    ArrayType* arrayType = new ArrayType( typeName );
-////    return arrayType;
-//}
 
 }
 
