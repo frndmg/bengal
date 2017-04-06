90b578890ef743477dcdd97f591b6f6e4a867a86
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-04-06 00:48:54 -0400

    Implement IfExpr::checkSemantic

--- ast/IfExpr.cpp
+++ ast/IfExpr.cpp
@@ -14,4 +14,41 @@ IfExpr::IfExpr(
 { }
 
 bool IfExpr::checkSemantic( Node::Scope& scope, Node::Report& report )
-{ }
+{
+    bool ok = true;
+
+    Scope if_nested_scope( &scope );
+
+    // IF
+    ok = if_()->checkSemantic( if_nested_scope, report ) and ok;
+
+    if ( not sameType( scope.getType( "int" ), { if_() } ) )
+    {
+        report.error( *this, "The condition is not a number" );
+        ok = false;
+    }
+
+    // THEN
+    ok = then_()->checkSemantic( if_nested_scope, report ) and ok;
+    auto then_type = then_()->type();
+
+    if ( else_() != nullptr )
+    {
+        Scope else_nested_scope( &scope );
+
+        // ELSE
+        ok = else_()->checkSemantic( else_nested_scope, report ) and ok;
+
+        auto else_type = else_()->type();
+
+        if ( then_type != else_type )
+            // If the type differs then none for the if
+            setType( scope.getType( "none" ) );
+        else
+            setType( then_type );
+    }
+    else
+        setType( then_type );
+
+    return ok;
+}
