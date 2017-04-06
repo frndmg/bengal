39583887cdb68154129e53038bcbc2a3d385b23f
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-04-06 00:36:42 -0400

    Format code. Add property accessors

--- ast/IfExpr.cpp
+++ ast/IfExpr.cpp
@@ -2,19 +2,16 @@
 
 using namespace ast;
 
-IfExpr::IfExpr(const std::shared_ptr<Expr>& cond,
-               const std::shared_ptr<Expr>& then,
-               const std::shared_ptr<Expr>& els,
-               const Position& pos) :
-    RValue( pos ),
-    m_cond(cond),
-    m_then(then),
-    m_else(els)
-{
+IfExpr::IfExpr(
+        const std::shared_ptr<Expr>& cond,
+        const std::shared_ptr<Expr>& then,
+        const std::shared_ptr<Expr>& els,
+        const Position& pos )
+        : RValue( pos )
+        , m_cond( cond )
+        , m_then( then )
+        , m_else( els )
+{ }
 
-}
-
-bool IfExpr::checkSemantic(Node::Scope &scope, Node::Report &report)
-{
-
-}
+bool IfExpr::checkSemantic( Node::Scope& scope, Node::Report& report )
+{ }
