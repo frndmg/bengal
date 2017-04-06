bb33977f816843eb57f72ca8b784bd1d6e7e4714
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-04-06 01:42:13 -0400

    Implement AssignExpr::checkSemantic

--- ast/AssignExpr.cpp
+++ ast/AssignExpr.cpp
@@ -14,8 +14,43 @@ AssignExpr::AssignExpr(
 
 bool AssignExpr::checkSemantic( Scope& scope, Report& report )
 {
-    bool valid_semantic = m_lvalue->checkSemantic( scope, report );
-    valid_semantic = m_expr->checkSemantic( scope, report ) and valid_semantic;
+    bool lvalue_check_semantic = m_lvalue->checkSemantic( scope, report );
+    bool expr_check_semantic = m_expr->checkSemantic( scope, report );
+    bool ok = lvalue_check_semantic and expr_check_semantic;
+
+    auto none_type = scope.getType( "none" );
+
+    if ( ok and m_lvalue->type() == nullptr )
+    {
+        // If the variable is new and the expression is well.
+        auto expr_type = m_expr->type();
+        if ( expr_type == none_type or expr_type == nullptr )
+        {
+            report.error( *m_expr, "Expression is not an rvalue" );
+            ok = false;
+        }
+        else
+        {
+            // Creates a new variable.
+            m_lvalue->setType( expr_type );
+            scope.addVariable( *m_lvalue->id(), expr_type );
+        }
+    }
+    else if ( ok )
+    {
+        // If the variable is not new.
+        auto expr_type = m_expr->type();
+        if ( expr_type != m_lvalue->type() )
+        {
+            report.error( *this, "The type of lvalue and expression mismatch" );
+            ok = false;
+        }
+    }
+
+    // This work against multiple assignations.
+    setType( none_type );
+
+    return ok;
 }
 
 ast::AssignExpr::operator std::string() const
