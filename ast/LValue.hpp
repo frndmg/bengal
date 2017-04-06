aa12c41d7b51bf3bf37783f9e3b55d55791e71f2
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-04-05 23:33:54 -0400

    Fix lvalue implementation

--- ast/LValue.hpp
+++ ast/LValue.hpp
@@ -1,6 +1,14 @@
 #ifndef LVALUE_HPP
 #define LVALUE_HPP
 
+#define LVALUE_INVALID_MEMBER_ACCESSOR "Invalid use of member accessor in a type that is not a struct"
+
+#define LVALUE_MISSING_MEMBER "Struct %s does not have a member named %s"
+
+#define LVALUE_INVALID_INDEX_ACCESSOR "Invalid index accessor over a no array type"
+
+#define LVALUE_INDEX_NOT_A_NUMBER "Index is not a number"
+
 #include "Id.hpp"
 #include "RValue.hpp"
 
@@ -8,44 +16,65 @@
 namespace ast
 {
 
-class LValue : public RValue
+class LValue
+        : public RValue
 {
 public:
-    ///
-    /// \brief Creates a single LValue with no member references
-    /// \param id  Pointer to a Id type
-    ///
-    LValue(const Position& pos = { 0, 0, 0, 0 });
+    /// \brief Creates an empty LValue.
+    LValue( const Position& pos = { 0, 0, 0, 0 } );
 
-    ///
-    /// \brief Creates a member accesor LValue
-    /// \param id     Pointer to a Id type
-    ///
-    LValue(std::shared_ptr<Id>& id, const Position& pos = { 0, 0, 0, 0 });
+    /// \brief Creates a member accessor.
+    /// \param id Pointer to Id type.
+    LValue(
+            const std::shared_ptr<Id>& id,
+            const Position& pos = { 0, 0, 0, 0 } );
 
-    ///
-    /// \brief Creates a index accesor LValue
-    /// \param index  Pointer to a Expr type
-    ///
-    LValue(std::shared_ptr<Expr>& index, const Position& pos = { 0, 0, 0, 0 });
+    /// \brief Creates an index accessor.
+    /// \param index Pointer to a Expr type.
+    LValue(
+            const std::shared_ptr<Expr>& index,
+            const Position& pos = { 0, 0, 0, 0 } );
 
-    void setNext(const std::shared_ptr<LValue>& next);
+    void setNext( const std::shared_ptr<LValue>& next );
 
-    bool isMemberAccesor() { return bool( m_id ); }
+    /// \brief Gets if the current LValue is a member accessor.
+    /// \return true if the current LValue is a member accessor,
+    ///         false otherwise.
+    bool isMemberAccessor();
 
-    bool isIndexAccesor() { return bool( m_index ); }
+    /// \brief Gets if the current LValue is index accessor.
+    /// \return true if the current LValue is a member accessor,
+    ///         false otherwise.
+    bool isIndexAccessor();
 
-    bool isSimple() { return not bool( m_next ); }
+    /// \brief Gets if the current LValue is empty.
+    /// \details If it is not a member accessor neither an index accessor.
+    /// \return true if the current LValue is a member accessor,
+    ///         false otherwise.
+    bool isEmpty();
 
-    // Node interface
+    const std::shared_ptr<Id>& id() const;
+
+    const std::shared_ptr<Expr>& index() const;
+
+    const std::shared_ptr<LValue>& next() const;
+
 public:
-    virtual bool checkSemantic(Scope& scope, Report& report) override;
+    virtual bool checkSemantic( Scope& scope, Report& report ) override;
+
+private:
+    bool checkSemantic(
+            std::shared_ptr<Type>& parent_type,
+            Scope& scope,
+            Node::Report& report );
+
+public:
 
     virtual operator std::string() const;
 
 private:
-    std::shared_ptr<Id>     m_id;
-    std::shared_ptr<Expr>   m_index;
+    std::shared_ptr<Id> m_id;
+    std::shared_ptr<Expr> m_index;
     std::shared_ptr<LValue> m_next;
 };
 
