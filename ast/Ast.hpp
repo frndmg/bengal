5c25dd98db17b8734ee8b2abe0b470801570d62b
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-04-05 20:30:26 -0400

    Improve ast implementation with the new scope type.

--- ast/Ast.hpp
+++ ast/Ast.hpp
@@ -1,13 +1,7 @@
-//
-// Created by frnd on 12/30/16.
-//
-
 #ifndef BENGAL_AST_HPP
 #define BENGAL_AST_HPP
 
 #include "Expr.hpp"
-
-#include <memory>
 
 namespace ast
 {
@@ -15,27 +9,39 @@ namespace ast
 class Ast
 {
 public:
-    Ast( const std::shared_ptr<Expr>& root ) : m_root( root ) {}
-    Ast( Expr* root = nullptr ) : m_root( root ) {}
+    /// \brief Creates a new instance with a root.
+    /// \param root The root of the ast.
+    Ast( const std::shared_ptr<Expr>& root )
+            : m_root( root )
+    { }
 
+    /// \brief Check the semantic of the ast.
+    /// \return true if the ast is well semantically or false otherwise.
     bool checkSemantic();
+
+    /// \brief Generate the code of the ast.
     void generateCode();
 
-    void setRoot( Expr* root ) { m_root = std::shared_ptr<Expr>( root ); }
-    void setRoot( const std::shared_ptr<Expr>& root ) { m_root = root; }
+    /// \brief Gets the root of the ast.
+    /// \return The root of the ast.
+    const std::shared_ptr< Expr >& root()
+    { return m_root; }
+
+    /// \brief Set the root of the ast.
+    /// \param root The new root of the ast.
+    void setRoot( const std::shared_ptr<Expr>& root )
+    { m_root = root; }
+
     void setRoot( const std::shared_ptr<Expr>&& root )
-    {
-        m_root = std::move( root );
-    }
+    { m_root = std::move( root ); }
 
     operator std::string() const;
 
     friend std::ostream& operator<<( std::ostream& out, const Ast& ast )
-    {
-        return out << static_cast<std::string>( ast );
-    }
+    { return out << static_cast<std::string>( ast ); }
 
 private:
+    /// \brief Root of the ast.
     std::shared_ptr<Expr> m_root;
 };
 
