eb1afe668e3f7fbb94c30bce49ed93c7ee649e0a
Fernando Martínez González <frndmartinezglez@gmail.com>
2017-04-05 20:38:51 -0400

    Improve node implementation

--- ast/Node.cpp
+++ ast/Node.cpp
@@ -3,16 +3,12 @@
 
 using namespace ast;
 
-Node::operator std::string() const
-{
-    return "Node()";
-}
-
 Node::Node( const Position& pos )
         : m_pos( pos )
 { }
 
 const Position& Node::position() const
-{
-    return m_pos;
-}
+{ return m_pos; }
+
+Node::operator std::string() const
+{ return "Node()"; }
